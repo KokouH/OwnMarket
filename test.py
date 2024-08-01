5055f963c5f714d6925ba313e5d453823acef3ec
KokouH <almazkymka@gmail.com>
2024-08-01 09:55:43 +0300

    Переписал тест нагрузки Теперь максимальный rps  5400 при инвентаре на 10 предметов Самое долго это получение строки инвенторя

--- test.py
+++ test.py
@@ -3,41 +3,44 @@ import time
 import random
 import requests
 import threading
+from multiprocessing import Pool
 
 s = []
 
-k = 2
-n = 10
-# n = random.randint(10, 30)
-results = [0 for i in range(k)]
-print(k, n)
+proc_count = 20
+threads_count = 2
+req_count = 250
+
+print("Start", proc_count * threads_count * req_count)
 
 # HTTP test
-def t(results, i):
+def th_func():
 
     st = time.time()
     c = 0
-    for _ in range(n):
+    for _ in range(req_count):
         try:
             r = requests.get('http://localhost:8484')
-            print(r.text)
+            # print(r.text)
             c += 1
         except:
             pass
         # print(r.text)
     print(time.time() - st, "sec time")
-    results[i] = c
 
-ts = []
-for i in range(k):
-    ts.append( threading.Thread(target=t, args=(results, i)) )
-    ts[-1].start()
-
-for thre in ts:
-    thre.join()
+def po(i):
+    ts = []
+    for _ in range(threads_count):
+        t = threading.Thread(target=th_func, daemon=True)
+        t.start()
+        ts.append(t)
     
-print(results)
-print(sum(results))
+    for t in ts:
+        t.join()
+
+with Pool(proc_count) as pool:
+    pool.map( po, [0 for _ in range(proc_count)] )
+
 
 # Socket test
 
